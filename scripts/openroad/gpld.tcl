ca9cba9d3d488395295219714992eff2803448d7
rustamc <rchochaev@gmail.com>
2022-11-02 15:58:19 +0300

    Update gpld.tcl

--- scripts/openroad/gpld.tcl
+++ scripts/openroad/gpld.tcl
@@ -12,25 +12,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if {![file exists $::env(MERGED_LEF)]} {
-    puts stderr "$::env(MERGED_LEF) doesn't exist!"
-    exit 1
+source $::env(SCRIPTS_DIR)/openroad/common/io.tcl
+read
+
+set ::block [[[::ord::get_db] getChip] getBlock]
+set ::insts [$::block getInsts]
+
+set placement_needed 0
+
+foreach inst $::insts {
+	if { ![$inst isFixed] } {
+		set placement_needed 1
+		break
+	}
 }
 
-if {![file exists $::env(CURRENT_DEF)]} {
-    puts stderr "$::env(CURRENT_DEF) doesn't exist!"
-    exit 1
+if { !$placement_needed } {
+	puts "\[WARN] All instances are FIXED/FIRM."
+	puts "\[WARN] No need to perform global placement."
+	puts "\[WARN] Skipping..."
+	write
+	exit 0
 }
 
 set json_conf "$::env(DESIGN_DIR)/dreamplace.json"
 file copy -force $::env(CURRENT_DEF) $::env(SAVE_DEF)
 
 set json_gen_cmd "python3 $::env(SCRIPTS_DIR)/config/dreamplace_conf.py \
-    $json_conf \
-    $::env(SAVE_DEF) \
-    $::env(MERGED_LEF) \
-    $::env(SAVE_DEF) \
-    $::env(placement_tmpfiles) \
+	$json_conf \
+	$::env(SAVE_DEF) \
+	$::env(MERGED_LEF) \
+	$::env(SAVE_DEF) \
+	$::env(placement_tmpfiles) \
 	$::env(PL_TARGET_DENSITY)"
 
 if { [catch {exec {*}$json_gen_cmd} errmsg] } {
@@ -52,7 +65,7 @@ switch status {
 	}
 	default {
 		puts stderr $errmsg
-    	#exit -1
+    	# exit -1
 	}
 }
 
@@ -61,7 +74,7 @@ if {1} {
 	# instead of /build/dreamplace/Placer.py:52
 	# path = "%s" % (params.result_dir)
 	# instead of /build/dreamplace/Placer.py:57
-    #         "%s.%s" % (params.design_name(), params.solution_file_suffix()))
+	#         "%s.%s" % (params.design_name(), params.solution_file_suffix()))
 	set save_def_basename [file rootname [file tail $::env(SAVE_DEF)]]
 	set out_def_name "$::env(placement_tmpfiles)/$save_def_basename/$save_def_basename.gp.def"
 	puts "\[INFO\] Out: $out_def_name"
@@ -70,43 +83,28 @@ if {1} {
 
 ## Standard part starts
 if {1} {
-	foreach lib $::env(LIB_SYNTH_COMPLETE) {
-		read_liberty $lib
-	}
-
-	if { [info exists ::env(EXTRA_LIBS) ] } {
-		foreach lib $::env(EXTRA_LIBS) {
-			read_liberty $lib
-		}
-	}
-
-	if {[catch {read_lef $::env(MERGED_LEF)} errmsg]} {
-		puts stderr $errmsg
-		exit 1
-	}
-
 	if {[catch {read_def $::env(SAVE_DEF)} errmsg]} {
 		puts stderr $errmsg
 		exit 1
 	}
 
 	if { $::env(PL_TIME_DRIVEN) } {
-		source $::env(SCRIPTS_DIR)/openroad/set_rc.tcl
-		read_sdc $::env(CURRENT_SDC)
+		source $::env(SCRIPTS_DIR)/openroad/common/set_rc.tcl
+		# read_sdc $::env(CURRENT_SDC)
 	}
 
 	if { $::env(PL_ROUTABILITY_DRIVEN) } {
-		source $::env(SCRIPTS_DIR)/openroad/set_routing_layers.tcl
+		source $::env(SCRIPTS_DIR)/openroad/common/set_routing_layers.tcl
 		set_macro_extension $::env(GRT_MACRO_EXTENSION)
-		source $::env(SCRIPTS_DIR)/openroad/layer_adjustments.tcl
+		source $::env(SCRIPTS_DIR)/openroad/common/set_layer_adjustments.tcl
 	}
 
 	if {[info exists ::env(CLOCK_PORT)]} {
 		if { $::env(PL_ESTIMATE_PARASITICS) == 1 } {
-			read_sdc -echo $::env(CURRENT_SDC)
+			#read_sdc -echo $::env(CURRENT_SDC)
 			unset_propagated_clock [all_clocks]
 			# set rc values
-			source $::env(SCRIPTS_DIR)/openroad/set_rc.tcl
+			source $::env(SCRIPTS_DIR)/openroad/common/set_rc.tcl
 			estimate_parasitics -placement
 
 			set ::env(RUN_STANDALONE) 0
